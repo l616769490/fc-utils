63bd636047864dd979be26f7e4f4e2fe18962e9b
unknown <ljemail@dingtalk.com>
2019-09-17 11:05:09 +0800

    0917

--- fcutils/__init__.py
+++ fcutils/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.3.0'
+__version__ = '0.3.6'
 
 from .http_utils import (
     getData, getDataForJson, getDataForStr, FCHttpUtils
@@ -11,4 +11,8 @@ from .oss_utils import (
 )
 from .pwd_utils import (
     createPwd, createSalt
+)
+
+from .sql_utils import (
+    joinList, pers, fieldStrFromList, fieldStr, fieldSplit, toJson
 )
